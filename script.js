5214fc98a008204c32cd3e91e51c8d7917c5fd07
Fawziyyah-hub <akinfalabif@gmail.com>
2024-07-03 22:02:47 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+
+    document.addEventListener("DOMContentLoaded",function(){
+        function updateTime(){
+            const now = new Date();
+            const utcTime=
+            now.toUTCString().split('')[4];
+            const day = now.toLocaleDateString('en-US',{weekday:'long'});
+
+            document.getElementById('current-time').textContent = utcTime;
+            document.getElementById('current-day').textContent = day;
+        }
+
+        updateTime();
+        setInterval(updateTime,1000);
+    });
+
+     
